b7fc17bfa95d4feecbdf73508c290452f2bb00f8
Janmejay Sahu <123504424+JAnmejayVASahu@users.noreply.github.com>
2024-03-30 16:13:21 +0530

    Update Question158.cpp
    158 basics of string

--- Question158.cpp
+++ Question158.cpp
@@ -6,15 +6,3 @@ int main(){
     cout << str;
     return 0;
 }
-
-// 159 reverse() function
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    string str = "Janmejay";
-    // cout << (int)str[3];
-    cout << str << endl;
-    reverse(str.begin(), str.end());
-    cout << str;
-    return 0;
-}
