e5c21efa9b2a715320fc85a295f3166242c6d587
Janmejay Sahu <123504424+JAnmejayVASahu@users.noreply.github.com>
2024-03-30 16:12:06 +0530

    Create Question158.cpp
    158 Basics of string

---
+++ Question158.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    getline(cin, str);
+    cout << str;
+    return 0;
+}
+
+// 159 reverse() function
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str = "Janmejay";
+    // cout << (int)str[3];
+    cout << str << endl;
+    reverse(str.begin(), str.end());
+    cout << str;
+    return 0;
+}
