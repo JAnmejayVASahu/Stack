37035b2f42578e88c5ed5b7944b374e843e75aa3
Janmejay Sahu <123504424+JAnmejayVASahu@users.noreply.github.com>
2024-03-30 16:15:06 +0530

    Create Question159.cpp
    159 reverse() function

---
+++ Question159.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str = "Janmejay";
+    // cout << (int)str[3];
+    cout << str << endl;
+    reverse(str.begin(), str.end());
+    cout << str;
+    return 0;
+}
